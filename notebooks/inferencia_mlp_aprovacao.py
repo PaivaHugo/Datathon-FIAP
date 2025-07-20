cd1a4400b25c46bc746c02d66a61d3bd84d9b26a
Amanda Brandão <amanda.brandao@g.globo>
2025-07-20 09:26:14 -0300

    Usando embeddings em algumas vars categóricas

---
+++ notebooks/inferencia_mlp_aprovacao.py
@@ -0,0 +1,137 @@
+
+import numpy as np
+import pandas as pd
+import joblib
+import re
+import shap
+from sentence_transformers import SentenceTransformer, util
+from tensorflow.keras.models import load_model
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Carregamento de modelos e pré-processadores
+model = load_model('modelo_mlp_aprovacao.h5')
+encoder = joblib.load('encoder_categorias.joblib')
+scaler = joblib.load('scaler_remuneracao.joblib')
+sbert = SentenceTransformer('neuralmind/bert-base-portuguese-cased')
+
+# Função utilitária para extrair remuneração
+def extrair_valor(texto):
+    match = re.search(r'(\d+[\.,]?\d*)', str(texto))
+    return float(match.group(1).replace(',', '.')) if match else 0.0
+
+def avalia_nivel_ingles_candidato_vs_vaga(nivel_vaga, nivel_candidato):
+    # print(f'VG: {nivel_vaga}')
+    # print(f'CAND: {nivel_candidato}')
+    nivel_ingles = {
+        0: 'Nenhum',
+        1: 'Básico',
+        2: 'Intermediário',
+        3: 'Técnico',
+        4: 'Avançado',
+        5: 'Fluente'
+    }
+    
+    n_vaga = 0
+    n_candidato = 0
+    for k, v in nivel_ingles.items():
+        if nivel_vaga == v:
+            n_vaga = k
+            break
+        
+    for k, v in nivel_ingles.items():
+        if nivel_candidato == "":
+            n_candidato = 0
+            break
+        if nivel_candidato == v:
+            n_candidato = k
+            break
+
+    if n_candidato >= n_vaga:
+        return 1
+    else:
+        return 0
+    
+def avalia_area_atuacao_candidato_vs_vaga(areas_vaga, areas_candidato):
+    match_area = 0
+    if areas_candidato != "":
+        a_candidato = areas_candidato.split(",")
+
+        
+        for i in a_candidato:
+            if i in areas_vaga:
+                match_area = 1
+                break
+    return match_area
+
+
+def avalia_nivel_profissional_candidato_vs_vaga(nivel_vaga, nivel_candidato):
+    if nivel_candidato == nivel_vaga:
+        return 1
+    else:
+        return 0
+
+# Função de predição para novo exemplo
+def prever_aprovacao(df_novo: pd.DataFrame):
+    # Geração de texto composto
+    df_novo = df_novo.T
+    # df_novo['texto_vaga'] = df_novo[[
+    #     'df_vg-principais_atividades',
+    #     'df_vg-competencia_tecnicas_e_comportamentais',
+    #     'df_vg-habilidades_comportamentais_necessarias'
+    # ]].fillna('').agg(' '.join, axis=1)
+
+    # df_novo['texto_candidato'] = df_novo[[
+    #     'df_applics-conhecimentos_tecnicos',
+    #     'df_applics-titulo_profissional'
+    # ]].fillna('').agg(' '.join, axis=1)
+
+    # # Embeddings e similaridades
+    # emb_vaga = sbert.encode(df_novo['texto_vaga'].tolist())
+    # emb_candidato = sbert.encode(df_novo['texto_candidato'].tolist())
+    # sim_vaga_candidato = np.diag(np.inner(emb_vaga, emb_candidato)).reshape(-1, 1)
+
+    # Categóricos
+    cat_cols = ['df_vg-vaga_sap', 'df_vg-vaga_especifica_para_pcd', 'df_vg-nivel profissional',
+                'df_vg-nivel_academico', 'df_vg-nivel_ingles', 'df_vg-nivel_espanhol',
+                'df_applics-pcd', 'df_applics-nivel_profissional', 'df_applics-nivel_academico',
+                'df_applics-nivel_ingles', 'df_applics-nivel_espanhol']
+    X_cat = encoder.transform(df_novo[cat_cols].fillna(''))
+
+    # Numérica
+    df_novo['df_applics-remuneracao_num'] = df_novo['df_applics-remuneracao'].apply(extrair_valor)
+    X_num = scaler.transform(df_novo[['df_applics-remuneracao_num']])
+
+    # Matches
+    df_novo["match_ingles"] = df_novo.apply(
+    lambda row: avalia_nivel_ingles_candidato_vs_vaga(row['df_vg-nivel_ingles'], row['df_applics-nivel_ingles']), axis=1)
+
+    df_novo["match_area_atuacao"] = df_novo.apply(
+    lambda row: avalia_area_atuacao_candidato_vs_vaga(row['df_vg-areas_atuacao'], row['df_applics-area_atuacao']), axis=1)
+
+    df_novo["match_nivel_profissional"] = df_novo.apply(
+    lambda row: avalia_nivel_profissional_candidato_vs_vaga(row['df_vg-nivel profissional'], row['df_applics-nivel_profissional']), axis=1)
+
+    X_match = df_novo[['match_ingles','match_area_atuacao','match_nivel_profissional']].values
+
+    # Concatenação final
+    #X = np.concatenate([emb_vaga, emb_candidato, sim_vaga_candidato, X_cat, X_num, X_match], axis=1)
+    X = np.concatenate([X_cat, X_num, X_match], axis=1)
+
+    # Predição
+    threshold=0.75
+    probas = model.predict(X)
+    classes = (probas > threshold).astype(int)
+
+    # explainer = shap.Explainer(model.predict, X)  # X_sample = 1 linha
+    # shap_values = explainer(X)
+    # shap.plots.waterfall(shap_values[0])
+
+    return pd.DataFrame({
+        'probabilidade_aprovacao': probas.ravel(),
+        'classe_prevista': classes.ravel()
+    })
+
+if __name__ == '__main__':
+    df_exemplo = pd.read_json('teste4.json')
+    resultado = prever_aprovacao(df_exemplo)
+    print(resultado)
