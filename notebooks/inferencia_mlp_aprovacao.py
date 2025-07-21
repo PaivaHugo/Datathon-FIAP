3cdbed9dc012d5f2c63fa4e8870d7629bb3b1a66
Amanda Brandão <amanda.brandao@g.globo>
2025-07-21 18:08:21 -0300

    Filtrando candidatos que tiveram preenchimento das colunas de interesse

--- notebooks/inferencia_mlp_aprovacao.py
+++ notebooks/inferencia_mlp_aprovacao.py
@@ -38,12 +38,12 @@ def avalia_nivel_ingles_candidato_vs_vaga(nivel_vaga, nivel_candidato):
             n_vaga = k
             break
         
-    for k, v in nivel_ingles.items():
+    for key, value in nivel_ingles.items():
         if nivel_candidato == "":
             n_candidato = 0
             break
-        if nivel_candidato == v:
-            n_candidato = k
+        if nivel_candidato == value:
+            n_candidato = key
             break
 
     if n_candidato >= n_vaga or ( n_vaga > n_candidato and (n_vaga - n_candidato) <= 2 ):
@@ -63,6 +63,29 @@ def avalia_area_atuacao_candidato_vs_vaga(areas_vaga, areas_candidato):
                 break
     return match_area
 
+def avalia_nivel_academico_candidato_vs_vaga(academico_vaga, academico_candidato):
+    nivel_academico = {
+        0: "Ensino Fundamental Incompleto",
+        1: "Ensino Fundamental Cursando",
+        2: "Ensino Fundamental Completo",
+        3: "Ensino Médio Incompleto",
+        4: "Ensino Médio Completo",
+        5: "Ensino Técnico Incompleto",
+        6: "Ensino Técnico Cursando",
+        7: "Ensino Técnico Completo",
+        8: "Ensino Superior Incompleto",
+        9: "Ensino Superior Cursando",
+        10: "Ensino Superior Completo",
+        11: "Pós Graduação Incompleto",
+        12: "Pós Graduação Cursando",
+        13: "Pós Graduação Completo",
+        14: "Mestrado Incompleto",
+        15: "Mestrado Cursando",
+        16: "Mestrado Completo",
+        17: "Doutorado Incompleto",
+        18: "Doutorado Cursando",
+        19: "Doutorado Completo"
+    }
 
 def avalia_nivel_profissional_candidato_vs_vaga(nivel_vaga, nivel_candidato):
     nivel_profissional = {
@@ -127,21 +150,21 @@ def avalia_nivel_academico_candidato_vs_vaga(nivel_vaga, nivel_candidato):
 def prever_aprovacao(df_novo: pd.DataFrame):
     # Geração de texto composto
     df_novo = df_novo.T
-    # df_novo['texto_vaga'] = df_novo[[
-    #     'df_vg-principais_atividades',
-    #     'df_vg-competencia_tecnicas_e_comportamentais',
-    #     'df_vg-habilidades_comportamentais_necessarias'
-    # ]].fillna('').agg(' '.join, axis=1)
+    df_novo['texto_vaga'] = df_novo[[
+        'df_vg-principais_atividades',
+        'df_vg-competencia_tecnicas_e_comportamentais',
+        'df_vg-habilidades_comportamentais_necessarias'
+    ]].fillna('').agg(' '.join, axis=1)
 
-    # df_novo['texto_candidato'] = df_novo[[
-    #     'df_applics-conhecimentos_tecnicos',
-    #     'df_applics-titulo_profissional'
-    # ]].fillna('').agg(' '.join, axis=1)
+    df_novo['texto_candidato'] = df_novo[[
+        'df_applics-conhecimentos_tecnicos',
+        'df_applics-cv_pt'
+    ]].fillna('').agg(' '.join, axis=1)
 
-    # # Embeddings e similaridades
-    # emb_vaga = sbert.encode(df_novo['texto_vaga'].tolist())
-    # emb_candidato = sbert.encode(df_novo['texto_candidato'].tolist())
-    # sim_vaga_candidato = np.diag(np.inner(emb_vaga, emb_candidato)).reshape(-1, 1)
+    # Embeddings e similaridades
+    emb_vaga = sbert.encode(df_novo['texto_vaga'].tolist())
+    emb_candidato = sbert.encode(df_novo['texto_candidato'].tolist())
+    similaridade_vaga_candidato = np.diag(np.inner(emb_vaga, emb_candidato)).reshape(-1, 1)
 
     # Categóricos
     cat_cols = ['df_vg-vaga_sap', 'df_vg-vaga_especifica_para_pcd', 'df_vg-nivel profissional',
@@ -155,7 +178,7 @@ def prever_aprovacao(df_novo: pd.DataFrame):
     X_num = scaler.transform(df_novo[['df_applics-remuneracao_num']])
 
     # Matches
-    df_novo["match_ingles"] = df_novo.apply(
+    df_novo["match_nivel_ingles"] = df_novo.apply(
     lambda row: avalia_nivel_ingles_candidato_vs_vaga(row['df_vg-nivel_ingles'], row['df_applics-nivel_ingles']), axis=1)
 
     df_novo["match_espanhol"] = df_novo.apply(
@@ -164,8 +187,8 @@ def prever_aprovacao(df_novo: pd.DataFrame):
     df_novo["match_area_atuacao"] = df_novo.apply(
     lambda row: avalia_area_atuacao_candidato_vs_vaga(row['df_vg-areas_atuacao'], row['df_applics-area_atuacao']), axis=1)
 
-    df_novo["match_nivel_profissional"] = df_novo.apply(
-    lambda row: avalia_nivel_profissional_candidato_vs_vaga(row['df_vg-nivel profissional'], row['df_applics-nivel_profissional']), axis=1)
+    df_novo["match_nivel_academico"] = df_novo.apply(
+    lambda row: avalia_nivel_academico_candidato_vs_vaga(row['df_vg-nivel_academico'], row['df_applics-nivel_academico']), axis=1)
 
     df_novo["match_nivel_academico"] = df_novo.apply(
     lambda row: avalia_nivel_academico_candidato_vs_vaga(row['df_vg-nivel_academico'], row['df_applics-nivel_academico']), axis=1)
@@ -174,8 +197,8 @@ def prever_aprovacao(df_novo: pd.DataFrame):
     X_match = df_novo[['match_ingles','match_espanhol','match_area_atuacao','match_nivel_profissional','match_nivel_academico']].values
 
     # Concatenação final
-    #X = np.concatenate([emb_vaga, emb_candidato, sim_vaga_candidato, X_cat, X_num, X_match], axis=1)
-    X = np.concatenate([X_cat, X_num, X_match], axis=1)
+    X = np.concatenate([emb_vaga, emb_candidato, similaridade_vaga_candidato, X_cat, X_num, X_match], axis=1)
+    #X = np.concatenate([X_cat, X_num, X_match], axis=1)
 
     # Predição
     threshold=0.75
